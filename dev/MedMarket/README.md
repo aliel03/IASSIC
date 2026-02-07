ea8e507da8bb8461f8c275e0acac219b01c4e2b2
aliel03 <151644632+aliel03@users.noreply.github.com>
2026-02-07 01:38:17 +0100

    Delete dev/MedMarket directory

--- dev/MedMarket/README.md
+++
@@ -1,2 +0,0 @@
-# MedMarket
-MedMarket is a MERN stack-based pharmaceutical e-commerce platform designed for seamless online shopping of medical products.
--- dev/MedMarket/utils/catch-async-err.js
+++
@@ -1,7 +0,0 @@
-function catchAsyncErr(func) {
-    return function (req, res, next) {
-        func(req, res, next).catch((error) => next(error));
-    };
-}
-
-module.exports = { catchAsyncErr };
